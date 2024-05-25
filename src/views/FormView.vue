8370a4cb86023053993b2326c14b3d18ed5d7b15
Than Phourac <thanphourac@Thans-MacBook-Pro.local>
2024-05-25 16:53:48 +0700

    using fetch api

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -6,8 +6,12 @@
     >
       <label for="name">Input name</label>
       <input v-model="name" />
-      <label for="gender">Input Phone Nnmber</label>
-      <input type="number" v-model="phone" />
+      <label for="email">Input email</label>
+      <input v-model="email" />
+      <label for="gender">Input gender</label>
+      <input v-model="gender" />
+      <label for="status">Input status</label>
+      <input v-model="status" />
       <input type="submit" />
     </form>
   </div>
@@ -18,56 +22,136 @@ export interface FormInput {
   name?: string;
   phone?: string;
 }
-import { computed, inject, reactive, ref, watch, type Ref } from "vue";
+import {
+  computed,
+  inject,
+  onMounted,
+  reactive,
+  ref,
+  watch,
+  type Ref,
+} from "vue";
 import { useRouter } from "vue-router";
-const formInput = inject<FormInput[]>("formInput");
+import type { IdataList } from "./ListView.vue";
+// const formInput = inject<FormInput[]>("formInput");
 const editId = inject<Ref<number>>("editId");
+
+const dataDetailList = ref<IdataList>({
+  email: "",
+  gender: "",
+  name: "",
+  status: "",
+});
+console.log("dataDetailList", dataDetailList);
+
+const createForm = async (formData: IdataList) => {
+  try {
+    const response = await fetch("https://gorest.co.in/public/v2/users", {
+      method: "POST", // specify the HTTP method (POST for creating data)
+      headers: {
+        "Content-Type": "application/json", // specify the content type of the request body
+        Authorization:
+          "Bearer caae78cf87de548147f33f97c2becc4a8b3c56d1550ca4407cd2e689b9689942",
+      },
+      body: JSON.stringify(formData), // convert the JavaScript object to a JSON string
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    console.log("Data created:", data); // handle the response data as needed
+  } catch (error) {
+    console.error("Failed to create data:", error);
+  }
+};
+
+const updateForm = async (formData: IdataList, id: number) => {
+  try {
+    const response = await fetch(`https://gorest.co.in/public/v2/users/${id}`, {
+      method: "PUT", // specify the HTTP method (POST for creating data)
+      headers: {
+        "Content-Type": "application/json", // specify the content type of the request body
+        Authorization:
+          "Bearer caae78cf87de548147f33f97c2becc4a8b3c56d1550ca4407cd2e689b9689942",
+      },
+      body: JSON.stringify(formData), // convert the JavaScript object to a JSON string
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    console.log("Data created:", data); // handle the response data as needed
+  } catch (error) {
+    console.error("Failed to create data:", error);
+  }
+};
+
+const dataDetail = async (id: number) => {
+  try {
+    const response = await fetch(`https://gorest.co.in/public/v2/users/${id}`, {
+      method: "GET", // specify the HTTP method (POST for creating data)
+      headers: {
+        "Content-Type": "application/json", // specify the content type of the request body
+        Authorization:
+          "Bearer caae78cf87de548147f33f97c2becc4a8b3c56d1550ca4407cd2e689b9689942",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    // dataDetailList.splice(0, dataDetailList.length, ...data); // Ensures reactivity by updating the array
+    dataDetailList.value = data;
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+  }
+};
+
+onMounted(() => {
+  if (editId?.value) {
+    dataDetail(editId.value);
+  }
+});
+
+watch(dataDetailList, (newData) => {
+  name.value = newData.name || "";
+  email.value = newData.email || "";
+  gender.value = newData.gender || "";
+  status.value = newData.status || "";
+});
 
 console.log("editIdForm", editId);
 
 const router = useRouter();
 
-console.log("formInput", formInput);
-const name = ref("");
-const phone = ref("");
-
-const defaultData = computed(() => {
-  return formInput?.find((item) => item.id === editId?.value);
-});
-
-watch(
-  defaultData,
-  (newData) => {
-    console.log("newData", newData);
-    if (newData) {
-      name.value = newData.name || "";
-      phone.value = newData.phone || "";
-    }
-  },
-  { immediate: true }
-);
-
-console.log("defaultData", defaultData);
+const name = ref<string>("");
+const gender = ref<string>("");
+const email = ref<string>("");
+const status = ref<string>("");
 
 function hanldeSubmit() {
-  if (name.value !== "" || phone.value !== "") {
-    if (editId?.value) {
-      const index = formInput?.findIndex((item) => item.id === editId.value);
-
-      if (index !== undefined && index !== -1 && formInput) {
-        formInput[index] = {
-          id: editId.value,
-          name: name.value,
-          phone: phone.value,
-        };
-      }
-    } else {
-      let id = Date.now();
-      formInput?.push({ name: name.value, phone: phone.value, id: id });
-    }
-
-    router.push("/");
+  if (editId) {
+    updateForm(
+      {
+        email: email.value,
+        gender: gender.value,
+        name: name.value,
+        status: status.value,
+      },
+      editId.value
+    );
   }
-  console.log("formInput", formInput);
+  createForm({
+    email: email.value,
+    gender: gender.value,
+    name: name.value,
+    status: status.value,
+  });
+
+  router.push("/");
 }
 </script>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,25 +1,79 @@
 <template>
   <h1>List</h1>
-  <ul v-for="(data, index) in filteredList" :key="data.id">
-    <li>{{ data.name }} {{ data.phone }}</li>
-    <button @click="handleEdit(data.id)">Edit</button>
-    <button @click="handleDelete(data.id)">Delete</button>
+  <ul v-for="(data, index) in dataList" :key="data.id">
+    <li>{{ data.name }} {{ data.gender }} {{ data.status }}</li>
+    <button @click="handleEdit(data.id || 0)">Edit</button>
+    <button @click="handleDelete(data.id || 0)">Delete</button>
   </ul>
 </template>
 
 <script setup lang="ts">
-import { computed, inject, ref, type Ref } from "vue";
+import { computed, inject, onMounted, reactive, ref, type Ref } from "vue";
 import type { FormInput } from "./FormView.vue";
 import { useRouter } from "vue-router";
 
+export interface IdataList {
+  id?: number;
+  name: string;
+  email: string;
+  gender: string;
+  status: string;
+}
+
+const dataList = reactive<IdataList[]>([]);
+
+console.log("dataList", dataList);
+
+const fetchData = async () => {
+  try {
+    const response = await fetch("https://gorest.co.in/public/v2/users", {
+      method: "GET", // specify the HTTP method (POST for creating data)
+      headers: {
+        "Content-Type": "application/json", // specify the content type of the request body
+        Authorization:
+          "Bearer caae78cf87de548147f33f97c2becc4a8b3c56d1550ca4407cd2e689b9689942",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    dataList.splice(0, dataList.length, ...data); // Ensures reactivity by updating the array
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+  }
+};
+
+const removeData = async (id: number) => {
+  try {
+    const response = await fetch(`https://gorest.co.in/public/v2/users/${id}`, {
+      method: "DELETE", // specify the HTTP method (POST for creating data)
+      headers: {
+        "Content-Type": "application/json", // specify the content type of the request body
+        Authorization:
+          "Bearer caae78cf87de548147f33f97c2becc4a8b3c56d1550ca4407cd2e689b9689942",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    dataList.splice(0, dataList.length, ...data); // Ensures reactivity by updating the array
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+  }
+};
+
+onMounted(fetchData);
+
 const removeId = ref(0);
-const formInput = inject<FormInput[]>("formInput");
+// const formInput = inject<FormInput[]>("formInput");
 const editId = inject<Ref<number>>("editId");
 // const formList = formInput?.filter((data) => data.id !== removeId.value);
 const router = useRouter();
-const filteredList = computed(() => {
-  return formInput?.filter((data) => data.id !== removeId.value) || [];
-});
+// const filteredList = computed(() => {
+//   return formInput?.filter((data) => data.id !== removeId.value) || [];
+// });
 
 function handleEdit(id: number) {
   console.log("id", id);
@@ -32,7 +86,8 @@ function handleEdit(id: number) {
 }
 function handleDelete(id: number) {
   console.log("id", id);
-  removeId.value = id;
-  console.log("filteredList", filteredList);
+  // removeId.value = id;
+  removeData(id);
+  // console.log("filteredList", filteredList);
 }
 </script>
